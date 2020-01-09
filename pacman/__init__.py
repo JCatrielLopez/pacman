a7d1586880a837c327a09e50aeee963076971b91
BautistaCRP <bj.carpintero@gmail.com>
2020-01-09 00:15:10 -0300

    TODO's resolved

--- pacman/__init__.py
+++ pacman/__init__.py
@@ -1,12 +1,3 @@
-# mode.py
-# TODO (30, 19): Posible bug, hay que mirarlo.
-# TODO (46, 3): Cambiar este modo para que no eliga una direccion aleatoria, si no una casilla objetivo al azar.
-
-# scene.py
-# TODO (181, 1): Definir bien la separacion entre escenas para evitar repetir codigo.
 
 # ghost.py
 # TODO (1, 1): Definir como mueren los fantasmas.
-
-# pacman.py
-# TODO (1, 1): No restar vidas si los fantasmas estan muertos.
