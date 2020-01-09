45fe406122957f23c228fe14a81a88af56153a31
BautistaCRP <bj.carpintero@gmail.com>
2020-01-09 00:13:56 -0300

    Notify functions added for async check support

--- pacman/actors/pacman.py
+++ pacman/actors/pacman.py
@@ -17,45 +17,57 @@ class Pacman(actor.MovingActor):
     sprites_down = None
     current_sprite = 0
 
-    def __init__(self, x, y, width, height, current_map=None, *groups):
+    # Updates
+    notify_scores = None
+    notify_lives = None
+    notify_pellets_in_map_change = None
+
+    def __init__(self, x, y, width, height, current_map, notify_scores, notify_lives, notify_pellets_in_map_change,
+                 *groups):
         super().__init__(
             x, y, width, height, constants.YELLOW, "../res/pacman", current_map, *groups
         )
+        self.notify_scores = notify_scores
+        self.notify_lives = notify_lives
+        self.notify_pellets_in_map_change = notify_pellets_in_map_change
 
-    def check_consumed(self, pellet_list):
-        energizer_consumed = False
+    def check_collision_pellets(self, pellet_list):
         colliding = [i for i in pellet_list.sprites() if self.rect.colliderect(i)]
-
         score = 0
         if len(colliding):
             for i in colliding:
                 if i.is_energizer():
-                    energizer_consumed = True
+                    self.power_up = True
                 score += i.get_score()
                 pellet_list.remove(i)
 
-        self.pellets_consumed += len(colliding)
-        self.power_up = energizer_consumed
-        return score, pellet_list
+            self.pellets_consumed += len(colliding)
+            self.add_score(score)
+            self.notify_pellets_in_map_change(pellet_list)
 
-    def add_score(self, score):
-        self.score += score
-
-    def get_score(self):
-        return self.score
-
-    def get_lives(self):
-        return self.lives
-
-    def check_collision(self, hitboxes):
+    def check_collision_ghosts(self, ghosts_hitboxes):
         hits = []
         collided = False
-        for hitbox in hitboxes:
+        for hitbox in ghosts_hitboxes:
             if self.rect.colliderect(hitbox):
                 collided = True
                 hits.append(hitbox)
 
         return collided, hits
+
+    def add_score(self, score):
+        self.score += score
+        self.notify_scores()
+
+    def get_score(self):
+        return self.score
+
+    def decrement_lives(self):
+        self.lives -= 1
+        self.notify_lives()
+
+    def get_lives(self):
+        return self.lives
 
     def get_consumed_amount(self):
         return self.pellets_consumed
